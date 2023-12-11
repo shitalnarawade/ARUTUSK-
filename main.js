afc62595f48ce3109f7517d6a612e2837b163b24
shitalnarawade <shital.narawade1@gmail.com>
2023-12-11 23:47:54 +0530

    this is my new commit1

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // console.log('This is js')
 console.log("This is my first commit");
 // 1d834bf
+
+// const userhead = document.getElementById('user_head');
+
+        // userhead.style.border = "solid 2px #000";
+         
+        // const item_1 =document.getElementById('item1');
+        // item_1.style.color ='solid 2px green';
+        
+        // const item = document.getElementById('item1');
+        // item.style.color = 'green';
+        // item.style.font = 'bold';
